34b6e328b7b62de423d482826e47940b74b03d50
Darlyn <cf21.darlyn.cevas27@iesjoandaustria.org>
2022-05-30 13:46:39 +0200

    hecho

---
+++ script.js
@@ -0,0 +1,6 @@
+const theme = document.getElementById('cambio-color');
+const navega = document.getElementById('navegador');
+
+theme.addEventListener('click', () =>{
+    navega.classList.add('white-bg');
+});
